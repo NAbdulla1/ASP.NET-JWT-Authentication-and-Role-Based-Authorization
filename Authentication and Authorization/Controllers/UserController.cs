2e7373db4162da848c757d3b3eb6e469db82f651
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2023-10-21 15:24:46 +0600

    Add project files.

---
+++ Authentication and Authorization/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Authentication_and_Authorization.Data;
+using Authentication_and_Authorization.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication_and_Authorization.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        private readonly UserContext _dbContext;
+
+        public UserController(UserContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([Bind("Email,Password")] User user)
+        {
+            user.UserType = UserType.Customer;
+            _dbContext.users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Register), user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([Bind("Email,Password")] User loginUser)
+        {
+            var user = _dbContext.users.Where(u => u.Email == loginUser.Email).FirstOrDefault();
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            if (user.Password != loginUser.Password)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ Authentication and Authorization/Data/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Authentication_and_Authorization.Data.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserType UserType { get; set; }
+    }
+
+    public enum UserType
+    {
+        Admin = 1, Customer 
+    }
+}
